75431de1e273139e53f40bcaaaea20b08ffab4c8
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-23 00:14:16 -0400

    Inital Commit

---
+++ Lab/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,2 @@
+def hello_world_test():
+    print("hello world")
---
+++ Lab/main.py
@@ -0,0 +1,10 @@
+import random
+from test import hello_world_test
+
+def rand():
+    return random.randrange(0, 10)
+
+def hello_world():
+    print("hello world")
+    
+test = hello_world_test
